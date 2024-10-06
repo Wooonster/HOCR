b5e46bfe35ca77ff92fea3f0dd605fcf1dd05645
Wonster <109267172+Wooonster@users.noreply.github.com>
2024-10-06 15:28:08 +1100

    Add dataset warnings in densenet_model.py

--- densenet_model.py
+++ densenet_model.py
@@ -2,6 +2,7 @@ import os
 import io
 import cv2
 import math
+import warnings
 import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
 import numpy as np
@@ -29,7 +30,9 @@ from torch.amp import autocast, GradScaler
 def prepare_datasets(data_pq_file):
     df = pd.read_parquet(data_pq_file)
     captions, img_names, img_bytes = df['formula'], df['filename'], df['image']
-    assert len(captions) == len(img_names) == len(img_bytes), 'dataset parquet got errors'
+    # assert len(captions) == len(img_names) == len(img_bytes), 'dataset parquet got errors'
+    if not len(captions) == len(img_names) == len(img_bytes):
+        warnings.warn('Warning! Dataset may got errors.')
     return df
 
 " customized tokenizer "
